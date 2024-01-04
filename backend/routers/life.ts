807fc0b9d1c6048508084fdbd8fab6556258c003
Your Name <you@example.com>
2024-01-04 16:06:41 +0900

    エラーハンドリングを追加

--- backend/routers/life.ts
+++ backend/routers/life.ts
@@ -7,27 +7,42 @@ import getRemainTimeForSeconds from "../utils/getRemainTimeForSecond";
 const router = express.Router();
 
 router.get("/lifespan", async (req: Request, res: Response) => {
-  const { year, sex } = req.query;
-  // yearとsexの存在チェック
-  if (!year || !sex) {
-    return res.status(400).json({ error: "性別と生年月日を選択してください" });
+  try {
+    const { year, sex } = req.query;
+    // yearとsexの存在チェック
+    if (!year || !sex) {
+      return res
+        .status(400)
+        .json({ error: "性別と生年月日を選択してください" });
+    }
+
+    // yearとsexの型チェック
+    if (typeof year !== "string" || typeof sex !== "string") {
+      return res
+        .status(400)
+        .json({ message: "性別と生年月日を選択してください" });
+    }
+
+    // 生年月日をDate型で取得→秒に変換
+    const birthDate = new Date(year);
+
+    const remainingTimeInSeconds = await getRemainTimeForSeconds(
+      sex,
+      birthDate
+    );
+
+    console.log("remainingTimeInSeconds: ", remainingTimeInSeconds);
+
+    return res.json({ remainTime: remainingTimeInSeconds });
+  } catch (err) {
+    if (err instanceof Error) {
+      // Errorオブジェクトの場合
+      return res.status(500).json({ message: err.message });
+    } else {
+      // それ以外の場合
+      return res.status(500).json({ message: "不明なエラーが発生しました" });
+    }
   }
-
-  // yearとsexの型チェック
-  if (typeof year !== "string" || typeof sex !== "string") {
-    return res
-      .status(400)
-      .json({ message: "性別と生年月日を選択してください" });
-  }
-
-  // 生年月日をDate型で取得→秒に変換
-  const birthDate = new Date(year);
-
-  const remainingTimeInSeconds = await getRemainTimeForSeconds(sex, birthDate);
-
-  console.log("remainingTimeInSeconds: ", remainingTimeInSeconds);
-
-  return res.json({ remainTime: remainingTimeInSeconds });
 });
 
 export default router;
